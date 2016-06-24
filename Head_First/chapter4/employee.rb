e95a37391b9587039b4b244fce30a8444c7b1ace
musaomondi <amokem@yahoo.com>
2016-06-24 11:49:18 +0300

    implement employee class and create new employee instances

---
+++ Head_First/chapter4/employee.rb
@@ -0,0 +1,26 @@
+class Employee
+  attr_reader :name, :salary
+
+  def name=(name)
+    if name==""
+      raise "Name can't be blank!"
+    end
+    @name=name
+  end
+  def salary=(salary)
+    if salary<0
+      raise "A salary of #{salary} is not valid"
+    end
+    @salary=salary
+  end
+  def print_pay_stub
+    puts "Name: #{@name}"
+    pay_for_period=(@salary/365)*14
+    puts "Pay This Period: $#{pay_for_period}"
+  end
+end
+
+musa=Employee.new
+musa.name="Musa"
+musa.salary=50000
+musa.print_pay_stub
