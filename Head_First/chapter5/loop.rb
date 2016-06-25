00b4ebc71f60cac108f9bc358e4fa06a4f41eade
musaomondi <amokem@yahoo.com>
2016-06-25 11:21:53 +0300

    subtract each price frm the customer balance in a given array of prices

--- Head_First/chapter5/loop.rb
+++ Head_First/chapter5/loop.rb
@@ -10,3 +10,14 @@ end
 prices=[3.99,25.00,8.99] #create the array holding the prices
 puts format("%.2f", total(prices)) #pass our array of prices to the method and format the result.
 
+def refund(prices)
+  amount=0 #the total starts at 0
+  index=0 #start at the first array
+  while index < prices.length #while we're still within the array
+    amount -= prices[index] #subtract the current price
+    index += 1 #move to the next price
+  end
+  amount #Return the refund
+end
+puts format("%.2f", refund(prices)) #pass our array of prices to the method and format the result.
+
