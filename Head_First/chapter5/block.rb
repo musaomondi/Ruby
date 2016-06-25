033a06cb7b189e796a0d862030ac9cbf8c2f1bd7
musaomondi <amokem@yahoo.com>
2016-06-25 12:18:12 +0300

    using parameters in a block

--- Head_First/chapter5/block.rb
+++ Head_First/chapter5/block.rb
@@ -16,3 +16,12 @@ end
 twice do
   puts "Woooooooo!"
 end
+
+def give(&my_block)
+  my_block.call("2 turtle doves", "1 partridge")
+end
+
+give do |present1, present2|
+  puts "My method gave me..."
+  puts present1, present2
+end
