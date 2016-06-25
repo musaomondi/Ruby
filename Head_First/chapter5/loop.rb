b8c2078f032c94c4f2dd1c5a870096a18c060567
musaomondi <amokem@yahoo.com>
2016-06-25 11:29:10 +0300

    reduce each items price by 1/3 and print the savings

--- Head_First/chapter5/loop.rb
+++ Head_First/chapter5/loop.rb
@@ -21,3 +21,12 @@ def refund(prices)
 end
 puts format("%.2f", refund(prices)) #pass our array of prices to the method and format the result.
 
+def show_discounts(prices)
+  index = 0 #start at the first array index
+  while index < prices.length #while still within the array
+    amount_off = prices[index]/3.0 #determine the discount for the current price
+    puts format("Your discount: $%.2f", amount_off) #format the discount
+    index += 1 #move to the next price
+  end
+end
+show_discounts(prices) #pass our array of prices to the method
