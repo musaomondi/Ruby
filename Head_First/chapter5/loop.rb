814b57c2962ee0a350747002ed80f213f5cbbe59
musaomondi <amokem@yahoo.com>
2016-06-25 13:18:07 +0300

    use each to refactor the 3methods and eliminate duplication

--- Head_First/chapter5/loop.rb
+++ Head_First/chapter5/loop.rb
@@ -1,32 +1,28 @@
 def total(prices)
   amount=0 #the total starts at 0
-  index=0 #start at the first array
-  while index < prices.length #while we're still within the array
-    amount += prices[index] #add the current price to the total
-    index += 1 #move to the next price
+  prices.each do |price| #process each price
+    amount+=price #add the current price to the total
   end
   amount #Return the total
 end
-prices=[3.99,25.00,8.99] #create the array holding the prices
-puts format("%.2f", total(prices)) #pass our array of prices to the method and format the result.
 
 def refund(prices)
   amount=0 #the total starts at 0
-  index=0 #start at the first array
-  while index < prices.length #while we're still within the array
-    amount -= prices[index] #subtract the current price
-    index += 1 #move to the next price
+  prices.each do |price| #process each price
+    amount-=price #Refund the current price
   end
   amount #Return the refund
 end
-puts format("%.2f", refund(prices)) #pass our array of prices to the method and format the result.
 
 def show_discounts(prices)
-  index = 0 #start at the first array index
-  while index < prices.length #while still within the array
-    amount_off = prices[index]/3.0 #determine the discount for the current price
+  prices.each do |price| #process each price
+    amount_off = price/3.0 #calculate discount
     puts format("Your discount: $%.2f", amount_off) #format the discount
-    index += 1 #move to the next price
   end
 end
+
+prices=[3.99,25.00,8.99] #create the array holding the prices
+
+puts format("%.2f", total(prices)) #pass our array of prices to the method and format the result.
+puts format("%.2f", refund(prices)) #pass our array of prices to the method and format the result.
 show_discounts(prices) #pass our array of prices to the method
