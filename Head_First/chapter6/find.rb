161273289f3e9746b32d3d9c6cf37cbb17d567b2
musaomondi <amokem@yahoo.com>
2016-06-27 12:15:32 +0300

    process each review, find the ajective and capitalize it

--- Head_First/chapter6/find.rb
+++ Head_First/chapter6/find.rb
@@ -13,3 +13,11 @@ end
 relevant_lines = lines.find_all { |line| line.include?("Truncated") }
 #exclude reviewer bylines
 reviews = relevant_lines.reject { |line| line.include?("--") }
+
+adjectives = reviews.map do |review| #process each review
+  adjective = find_adjective(review) #find the adjective
+  "'#{adjective.capitalize}'"
+end
+
+puts "The critics agree, Truncated is:"
+puts adjectives
