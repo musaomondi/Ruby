98f1e5115a0bc19199db5579939a3e3e8f31669a
musaomondi <amokem@yahoo.com>
2016-06-27 12:10:01 +0300

    find lines that include the movie name and exlude the bylines

--- Head_First/chapter6/find.rb
+++ Head_First/chapter6/find.rb
@@ -8,3 +8,8 @@ lines = [] #create this variable outside the class
 File.open("reviews.txt") do |review_file| #open file and automatically close it
 lines=review.readlines #read every line in the file into an array
 end
+
+#find lines that include the movie name (Truncated)
+relevant_lines = lines.find_all { |line| line.include?("Truncated") }
+#exclude reviewer bylines
+reviews = relevant_lines.reject { |line| line.include?("--") }
