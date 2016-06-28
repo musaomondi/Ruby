4378efe97bb0ec66f95dfe6d1f334483f3a2cab8
musaomondi <amokem@yahoo.com>
2016-06-28 12:23:24 +0300

    normalizing hash keys

--- Head_First/chapter7/tally.rb
+++ Head_First/chapter7/tally.rb
@@ -8,6 +8,7 @@ end
 votes = Hash.new(0) #create a new hash with a default object of "0"
 lines.each do |line|
   name = line.chomp #remove the newline character
+  name.upcase! #change the name to all caps before using it as a hash key
     votes[name] +=1 #increment whatever the value is returned
 end
 
