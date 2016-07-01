de39f3f1272a158992aea728f2be1e4e8ca77b0b
musaomondi <amokem@yahoo.com>
2016-07-01 09:58:27 +0300

    adding own documentation

--- Head_First/chapter10/word_splitter.rb
+++ Head_First/chapter10/word_splitter.rb
@@ -1,9 +1,14 @@
+#This class allows you to perfom various operations
+#on the words in a string
 class WordSplitter
 
   include Enumerable
 
+  #the string to be split into words
   attr_accessor :string
   
+  #Passes each word in the string to a block, one 
+  #at a time
   def each
     string.split("").each do |word|
       yield word
