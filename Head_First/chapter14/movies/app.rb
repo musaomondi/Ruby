e31ee65c028539719bc0941d0b3327ca93946425
musaomondi <amokem@yahoo.com>
2016-07-03 08:57:37 +0300

    set up a movie object in the sinatra app

--- Head_First/chapter14/movies/app.rb
+++ Head_First/chapter14/movies/app.rb
@@ -1,5 +1,9 @@
 require 'sinatra'
+require 'movie'
+
 get('/movies') do
+  @movie = Movie.new #set up a new movie object
+  @movie.title = "Jaws"
   erb :index #load "views/index.erb"
   'Coming Soon...'
 end
