97bb381605b758fca5daa081dc87b2d152b525dc
musaomondi <amokem@yahoo.com>
2016-07-08 12:24:41 +0300

    combing strings using + operator

---
+++ Learn_ruby/Ex7.rb
@@ -0,0 +1,23 @@
+puts "Mary had a little lamb."
+#interpolates the string snow into the string
+puts "Its fleece was white as #{'snow'}."
+#prints a string and a new line
+puts "And everywhere that Mary went."
+#prints . ten times
+puts "." * 10 
+#assigns a string to a variable
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+#combines the strings
+print end1 + end2 + end3 + end4 + end5 + end6
+puts end7 + end8 + end9 + end10 + end11 + end12
